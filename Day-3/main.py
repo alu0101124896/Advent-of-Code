427599314f2d99b5b8d063831ecd209ca9f90c65
Sergio Tabares <alu0101124896@ull.edu.es>
2020-12-13 18:06:00 +0000

    Day 3 - Second part completed

--- Day-3/main.py
+++ Day-3/main.py
@@ -1,3 +1,5 @@
+import numpy
+
 file_name = input("\nInput file: ")
 
 f = open(file_name, 'r')
@@ -6,13 +8,16 @@ trees_map = f.read().split("\n")
 if trees_map[len(trees_map) - 1] == '':
   trees_map.pop()
 
-print("\nSlope:")
-horizontal_displacement = int(input("Horizontal displacement: "))
-vertical_displacement = int(input("Vertical displacement: "))
+# print("\nSlope:")
+# horizontal_displacement = int(input("Horizontal displacement: "))
+# vertical_displacement = int(input("Vertical displacement: "))
+slopes_list = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
 
-print("\nInitial position:")
-x_coordinate = int(input("X coordinate: "))
-y_coordinate = int(input("Y coordinate: "))
+# print("\nInitial position:")
+# x_coordinate = int(input("X coordinate: "))
+# y_coordinate = int(input("Y coordinate: "))
+x_coordinate = 0
+y_coordinate = 0
 
 
 def slideDownhill(trees_map,
@@ -35,7 +40,15 @@ def slideDownhill(trees_map,
                          number_of_trees)
 
 
-number_of_trees = slideDownhill(trees_map, x_coordinate, y_coordinate,
-                                horizontal_displacement, vertical_displacement)
+def slideDownhillForEachSlope(slope):
+  return slideDownhill(trees_map, x_coordinate, y_coordinate, slope[0],
+                       slope[1])
 
-print("\nNumber of trees encountered:", number_of_trees)
+
+# number_of_trees = slideDownhill(trees_map, x_coordinate, y_coordinate,
+#                                 horizontal_displacement, vertical_displacement)
+number_of_trees_list = list(map(slideDownhillForEachSlope, slopes_list))
+
+# print("\nNumber of trees encountered:", number_of_trees)
+print("\nMultiplication of all trees encountered on every slope checked:",
+      numpy.prod(number_of_trees_list))
