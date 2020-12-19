5da6e55ea6020584f7fd57d525e71d9fbe01e766
Sergio Tabares <alu0101124896@ull.edu.es>
2020-12-19 15:45:38 +0000

    Day 6 - Second part completed

--- Day-6/main.py
+++ Day-6/main.py
@@ -14,3 +14,41 @@ numberOfDistinctYesAnswersByGroup = list(
 print(
     "\nThe sum of the number of questions to which anyone answered \"yes\" on each group is:",
     sum(numberOfDistinctYesAnswersByGroup))
+
+allYesAnswersByGroupAndPerson = list(
+    map(
+        lambda yesAnswersByGroupAndPerson: yesAnswersByGroupAndPerson.split(
+            "\n"), allYesAnswers))
+
+if allYesAnswersByGroupAndPerson[len(allYesAnswersByGroupAndPerson) - 1][
+    len(allYesAnswersByGroupAndPerson[len(allYesAnswersByGroupAndPerson) - 1]) -
+    1] == "":
+  allYesAnswersByGroupAndPerson[len(allYesAnswersByGroupAndPerson) - 1].pop()
+
+allYesAnswersByGroupAndPerson = list(
+    map(
+        lambda yesAnswersByGroupAndPerson: list(
+            map(lambda param: set(param), yesAnswersByGroupAndPerson)),
+        allYesAnswersByGroupAndPerson))
+
+
+def commonYesAnswers(yesAnswersByGroupAndPerson):
+  commonYesAnswersByGroup = yesAnswersByGroupAndPerson[0]
+  for yesAnswersByPerson in yesAnswersByGroupAndPerson:
+    commonYesAnswersByGroup = commonYesAnswersByGroup.intersection(
+        yesAnswersByPerson)
+  return commonYesAnswersByGroup
+
+
+allCommonYesAnswersByGroup = list(
+    map(
+        lambda yesAnswersByGroupAndPerson: commonYesAnswers(
+            yesAnswersByGroupAndPerson), allYesAnswersByGroupAndPerson))
+
+numberOfCommonYesAnswersByGroup = list(
+    map(lambda commonYesAnswersByGroup: len(commonYesAnswersByGroup),
+        allCommonYesAnswersByGroup))
+
+print(
+    "\nThe sum of the number of questions to which everyone answered \"yes\" on each group is:",
+    sum(numberOfCommonYesAnswersByGroup))
