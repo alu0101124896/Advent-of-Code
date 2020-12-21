badca0caccc48ece58c608c955897fa34e665bc6
Sergio Tabares <alu0101124896@ull.edu.es>
2020-12-21 21:55:57 +0000

    Day 8 - Second part completed

--- Day-8/main.py
+++ Day-8/main.py
@@ -16,31 +16,75 @@ instructions = list(
 
 
 def detectInfiniteLoop(instructions, instructionIndex=0, accumulator=0):
+  if instructionIndex >= len(instructions):
+    return accumulator, False
+
   if instructions[instructionIndex].get("executed") == True:
-    return accumulator
+    return accumulator, True
 
   else:
     instructions[instructionIndex].update({"executed": True})
 
-    operation = instructions[instructionIndex].get("operation")
-    argument = int(instructions[instructionIndex].get("argument"))
+    currentOperation = instructions[instructionIndex].get("operation")
+    currentArgument = instructions[instructionIndex].get("argument")
 
-    if operation == 'acc':
-      accumulator += argument
+    if currentOperation == 'acc':
+      accumulator += int(currentArgument)
       instructionIndex += 1
 
-    elif operation == 'jmp':
-      instructionIndex += argument
+    elif currentOperation == 'jmp':
+      instructionIndex += int(currentArgument)
 
-    elif operation == 'nop':
+    elif currentOperation == 'nop':
       instructionIndex += 1
 
     else:
-      print("Error: unknown operation")
-      return -1
+      raise Exception(
+          f"Unknown operation '{currentOperation} {currentArgument}' at line {instructionIndex + 1}"
+      )
 
     return detectInfiniteLoop(instructions, instructionIndex, accumulator)
 
 
-print("\nThe value in the accumulator before starting the infinite loop is:",
-      detectInfiniteLoop(instructions))
+def copyOf(instructions):
+  return list(map(lambda instruction: instruction.copy(), instructions))
+
+
+try:
+  print("\nThe value in the accumulator before starting the infinite loop is:",
+        detectInfiniteLoop(copyOf(instructions))[0])
+except Exception as error:
+  print("Error:", error.args[0])
+  raise
+
+
+def swapOperation(instructions, instructionIndex, currentOperation):
+  if currentOperation == 'nop':
+    instructions[instructionIndex].update({"operation": 'jmp'})
+  elif currentOperation == 'jmp':
+    instructions[instructionIndex].update({"operation": 'nop'})
+
+
+def fixCorruptedInstruction(instructions, instructionIndex=0):
+  if instructionIndex >= len(instructions):
+    raise Exception("\nNo corruption detected")
+
+  else:
+    swapOperation(instructions, instructionIndex,
+                  instructions[instructionIndex].get("operation"))
+    accumulator, infiniteLoopDetected = detectInfiniteLoop(copyOf(instructions))
+
+    if not infiniteLoopDetected:
+      return accumulator
+
+    else:
+      swapOperation(instructions, instructionIndex,
+                    instructions[instructionIndex].get("operation"))
+      return fixCorruptedInstruction(instructions, instructionIndex + 1)
+
+
+try:
+  print("\nThe value of the accumulator after the program terminates is:",
+        fixCorruptedInstruction(copyOf(instructions)))
+except Exception as error:
+  print(error.args[0])
