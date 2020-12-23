8a5d9a9176b90842b451aea064fba638254e33d1
Sergio Tabares <alu0101124896@ull.edu.es>
2020-12-23 12:39:26 +0000

    Day 10 - Second part completed

--- Day-10/main.py
+++ Day-10/main.py
@@ -13,14 +13,42 @@ def main():
   diferencesList = ratingDiferences(adaptersList)
 
   print("\nThe result is:", diferencesList.count(1) * diferencesList.count(3))
+  print("\nThe total number of distinct arrangements for the adapters is:",
+        distinctArrangements(diferencesList))
 
 
 def ratingDiferences(adaptersList):
   diferencesList = list()
+
   for adaptersListIndex in range(1, len(adaptersList)):
     diferencesList.append(adaptersList[adaptersListIndex] -
                           adaptersList[adaptersListIndex - 1])
+
   return diferencesList
+
+
+def distinctArrangements(diferencesList):
+  diferencesList = "".join(map(str, diferencesList)).split("3")
+  totalCombinations = 1
+
+  for adapt in diferencesList:
+    numberOfAdapter = len(adapt)
+
+    if numberOfAdapter > 1:
+      totalCombinations *= combination(numberOfAdapter - 1)
+
+  return totalCombinations
+
+
+def combination(numberOfAdapter):
+  if numberOfAdapter < 3:
+    return 2**numberOfAdapter
+
+  elif numberOfAdapter == 3:
+    return (2**numberOfAdapter) - 1
+
+  else:
+    return combination(numberOfAdapter - 1) - 1
 
 
 main()
