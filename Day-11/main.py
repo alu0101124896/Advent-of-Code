d61f3e79034353525d377bbcc87b2f80ef259549
Sergio Tabares <alu0101124896@ull.edu.es>
2020-12-24 01:08:22 +0000

    Day 11 - Second part completed

--- Day-11/main.py
+++ Day-11/main.py
@@ -10,7 +10,8 @@ def main():
   for row in seatLayout:
     filledSeatLayout.append(row[:])
 
-  fillFerry(filledSeatLayout)
+  # fillFerry(filledSeatLayout)
+  fillFerryV2(filledSeatLayout)
   print("\nAt the end,", countOccupiedSeats(filledSeatLayout),
         "seats have beed occupied.")
 
@@ -85,6 +86,141 @@ def countOccupiedSeats(seatLayout):
         totalOccupiedSeats += 1
 
   return totalOccupiedSeats
+
+
+def fillFerryV2(seatLayout):
+  while True:
+    prevSeatLayout = list()
+    seatLayoutChanged = False
+
+    for row in seatLayout:
+      prevSeatLayout.append(row[:])
+
+    for rowIndex in range(len(seatLayout)):
+      for seatIndex in range(len(seatLayout[rowIndex])):
+        visualyOccupiedSeatsArround = hasVisualyOccupiedSeatsArround(
+            rowIndex, seatIndex, prevSeatLayout)
+
+        if seatLayout[rowIndex][
+            seatIndex] == 'L' and not visualyOccupiedSeatsArround:
+          seatLayout[rowIndex][seatIndex] = '#'
+          seatLayoutChanged = True
+
+        elif seatLayout[rowIndex][
+            seatIndex] == '#' and visualyOccupiedSeatsArround >= 5:
+          seatLayout[rowIndex][seatIndex] = 'L'
+          seatLayoutChanged = True
+
+    if not seatLayoutChanged:
+      break
+
+  return seatLayout
+
+
+def hasVisualyOccupiedSeatsArround(rowIndex, seatIndex, seatLayout):
+  occupiedSeats = 0
+
+  upRowIndex = rowIndex
+  leftSeatIndex = seatIndex
+  rightSeatIndex = seatIndex
+
+  sawnLeftUp = False
+  sawnUp = False
+  sawnRightUp = False
+
+  while upRowIndex > 0:
+    if not sawnLeftUp and leftSeatIndex > 0:
+      if seatLayout[upRowIndex - 1][leftSeatIndex - 1] == '#':
+        occupiedSeats += 1
+        sawnLeftUp = True
+
+      elif seatLayout[upRowIndex - 1][leftSeatIndex - 1] == 'L':
+        sawnLeftUp = True
+
+    if not sawnUp:
+      if seatLayout[upRowIndex - 1][seatIndex] == '#':
+        occupiedSeats += 1
+        sawnUp = True
+
+      elif seatLayout[upRowIndex - 1][seatIndex] == 'L':
+        sawnUp = True
+
+    if not sawnRightUp and rightSeatIndex < len(seatLayout[rowIndex]) - 1:
+      if seatLayout[upRowIndex - 1][rightSeatIndex + 1] == '#':
+        occupiedSeats += 1
+        sawnRightUp = True
+
+      elif seatLayout[upRowIndex - 1][rightSeatIndex + 1] == 'L':
+        sawnRightUp = True
+
+    upRowIndex -= 1
+    leftSeatIndex -= 1
+    rightSeatIndex += 1
+
+  downRowIndex = rowIndex
+  leftSeatIndex = seatIndex
+  rightSeatIndex = seatIndex
+
+  sawnLeftDown = False
+  sawnDown = False
+  sawnRightDown = False
+
+  while downRowIndex < len(seatLayout) - 1:
+    if not sawnLeftDown and leftSeatIndex > 0:
+      if seatLayout[downRowIndex + 1][leftSeatIndex - 1] == '#':
+        occupiedSeats += 1
+        sawnLeftDown = True
+
+      elif seatLayout[downRowIndex + 1][leftSeatIndex - 1] == 'L':
+        sawnLeftDown = True
+
+    if not sawnDown:
+      if seatLayout[downRowIndex + 1][seatIndex] == '#':
+        occupiedSeats += 1
+        sawnDown = True
+
+      elif seatLayout[downRowIndex + 1][seatIndex] == 'L':
+        sawnDown = True
+
+    if not sawnRightDown and rightSeatIndex < len(seatLayout[rowIndex]) - 1:
+      if seatLayout[downRowIndex + 1][rightSeatIndex + 1] == '#':
+        occupiedSeats += 1
+        sawnRightDown = True
+
+      elif seatLayout[downRowIndex + 1][rightSeatIndex + 1] == 'L':
+        sawnRightDown = True
+
+    downRowIndex += 1
+    leftSeatIndex -= 1
+    rightSeatIndex += 1
+
+  leftSeatIndex = seatIndex
+
+  while leftSeatIndex > 0:
+    if seatLayout[rowIndex][leftSeatIndex - 1] == '#':
+      occupiedSeats += 1
+      break
+
+    elif seatLayout[rowIndex][leftSeatIndex - 1] == 'L':
+      break
+
+    else:
+      leftSeatIndex -= 1
+
+  rightSeatIndex = seatIndex
+
+  while rightSeatIndex < len(seatLayout[rowIndex]) - 1:
+    if seatLayout[rowIndex][rightSeatIndex + 1] == '#':
+      occupiedSeats += 1
+      break
+
+    elif seatLayout[rowIndex][rightSeatIndex + 1] == 'L':
+      break
+
+    else:
+      rightSeatIndex += 1
+
+  return occupiedSeats
 
 
 main()
