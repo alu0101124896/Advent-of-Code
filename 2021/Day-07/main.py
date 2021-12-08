c314241b306bf8952507079e86cd859d12a2de2b
Sergio Tabares <alu0101124896@ull.edu.es>
2021-12-08 18:29:24 +0000

    2021 - Day 07 - Second part completed

--- 2021/Day-07/main.py
+++ 2021/Day-07/main.py
@@ -5,13 +5,20 @@ Since: December 2021
 Description: This program implemets my solution to the Advent of Code challenge.
 '''
 
+import math
+
 
 def main():
     '''Main function to resolve the challenge.'''
     crab_positions = parse_data()
 
-    target_position = align_crabs(crab_positions)
-    spent_fuel = calculate_spent_fuel(crab_positions, target_position)
+    # target_position = align_crabs(crab_positions)
+    # spent_fuel = calculate_spent_fuel(crab_positions, target_position)
+
+    target_position_1, target_position_2 = align_crabs_v2(crab_positions)
+    spent_fuel_1 = calculate_spent_fuel_v2(crab_positions, target_position_1)
+    spent_fuel_2 = calculate_spent_fuel_v2(crab_positions, target_position_2)
+    spent_fuel = min(spent_fuel_1, spent_fuel_2)
 
     print("\nThe least fuel possible they must spend is:", spent_fuel)
 
@@ -53,6 +60,26 @@ def calculate_spent_fuel(crab_positions, target_position):
     spent_fuel = 0
     for position, crabs in crab_positions.items():
         spent_fuel += abs(target_position - position) * crabs
+
+    return spent_fuel
+
+
+def align_crabs_v2(crabs_positions):
+    '''Function to align crabs using the least fuel possible.'''
+    crab_positions_sorted = list()
+    for position in range(max(crabs_positions.keys()) + 1):
+        crab_positions_sorted.extend([position] *
+                                     crabs_positions.get(position, 0))
+
+    mean_position = sum(crab_positions_sorted) / len(crab_positions_sorted)
+    return math.ceil(mean_position), math.floor(mean_position)
+
+
+def calculate_spent_fuel_v2(crab_positions, target_position):
+    '''Function to calculate the fuel spent by every crab.'''
+    spent_fuel = 0
+    for position, crabs in crab_positions.items():
+        spent_fuel += sum(range(abs(target_position - position) + 1)) * crabs
 
     return spent_fuel
 
