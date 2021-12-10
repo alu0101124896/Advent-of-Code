7e57a76ef598c560180b0358fbe3eece2f93e9d5
Sergio Tabares <alu0101124896@ull.edu.es>
2021-12-10 17:28:07 +0000

    2021 - Day 10 - Second part Completed

--- 2021/Day-10/main.py
+++ 2021/Day-10/main.py
@@ -10,9 +10,11 @@ def main():
     '''Main function to resolve the challenge.'''
     navigation_subsystem_code = parse_data()
 
-    syntax_error_score = corrupted_data(navigation_subsystem_code)
+    # syntax_error_score = corrupted_data(navigation_subsystem_code)
+    _, competition_middle_score = corrupted_data(navigation_subsystem_code)
 
-    print("\nThe total syntax error score is:", syntax_error_score)
+    # print("\nThe total syntax error score is:", syntax_error_score)
+    print("\nThe competition middle score is:", competition_middle_score)
 
 
 def parse_data():
@@ -30,6 +32,7 @@ def corrupted_data(code):
     '''Function to calculate the syntax error score of the navigation subsystem
     code.'''
     syntax_error_score = 0
+    total_scores = []
     for line in code:
         open_brackets = []
         for char in line:
@@ -38,15 +41,19 @@ def corrupted_data(code):
             elif char in [')', ']', '}', '>']:
                 if char == ')' and open_brackets[-1] != '(':
                     syntax_error_score += 3
+                    open_brackets.clear()
                     break
                 elif char == ']' and open_brackets[-1] != '[':
                     syntax_error_score += 57
+                    open_brackets.clear()
                     break
                 elif char == '}' and open_brackets[-1] != '{':
                     syntax_error_score += 1197
+                    open_brackets.clear()
                     break
                 elif char == '>' and open_brackets[-1] != '<':
                     syntax_error_score += 25137
+                    open_brackets.clear()
                     break
                 else:
                     open_brackets.pop()
@@ -54,7 +61,25 @@ def corrupted_data(code):
                 raise SyntaxError("Error: expected any kind of bracket," +
                                   f" but found '{char}' instead")
 
-    return syntax_error_score
+        if len(open_brackets) > 0:
+            line_total_score = 0
+            open_brackets.reverse()
+            for bracket in open_brackets:
+                line_total_score *= 5
+                if bracket == '(':
+                    line_total_score += 1
+                if bracket == '[':
+                    line_total_score += 2
+                if bracket == '{':
+                    line_total_score += 3
+                if bracket == '<':
+                    line_total_score += 4
+            total_scores.append(line_total_score)
+
+    total_scores.sort()
+    middle_score = total_scores[len(total_scores) // 2]
+
+    return syntax_error_score, middle_score
 
 
 if __name__ == "__main__":
