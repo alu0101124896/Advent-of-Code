ce5df827e9faf2e0264c2c916f8ca23f44312d34
Sergio Tabares <alu0101124896@ull.edu.es>
2022-12-09 22:23:39 +0000

    2022 - Day 07 - Second part completed

--- 2022/Day-07/main.py
+++ 2022/Day-07/main.py
@@ -21,6 +21,13 @@ def main():
 
     print("  The total size is:", part_one_solution)
 
+    print("\nPart two:")
+
+    (directory_to_delete,
+     part_two_solution) = get_directory_to_delete(filesystem)
+
+    print("  The total size is:", part_two_solution)
+
 
 def parse_data():
     """Function to parse the input data of the challenge."""
@@ -50,6 +57,25 @@ def get_directory_sizes(filesystem, max_size=None):
             directory_sizes.append(current_directory_size)
 
     return directory_sizes
+
+
+def get_directory_to_delete(filesystem):
+    """Function to get the smallest directory to free enough space to update
+    the system."""
+
+    total_available_space = 70000000
+    needed_free_space = 30000000
+    current_free_space = total_available_space - filesystem.get_total_size()
+
+    candidate_directories = []
+    for current_directory in filesystem.get_all_directories():
+        current_directory_size = current_directory.get_total_size()
+
+        if (current_free_space + current_directory_size) > needed_free_space:
+            candidate_directories.append((current_directory,
+                                         current_directory_size))
+
+    return min(candidate_directories, key=lambda candidate: candidate[1])
 
 
 if __name__ == "__main__":
