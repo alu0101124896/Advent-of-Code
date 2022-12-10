f6eee11a932723fa89ff225245e881f048261d33
Sergio Tabares <alu0101124896@ull.edu.es>
2022-12-10 15:12:40 +0000

    2022 - Day 08 - Second part completed

--- 2022/Day-08/main.py
+++ 2022/Day-08/main.py
@@ -18,6 +18,12 @@ def main():
 
     print("  The number of visible trees is:", part_one_solution)
 
+    print("\nPart two:")
+
+    part_two_solution = get_best_scenic_score(trees_grid)
+
+    print("  The best scenic score is:", part_two_solution)
+
 
 def parse_data():
     """Function to parse the input data of the challenge."""
@@ -65,6 +71,65 @@ def is_visible(row, col, grid):
 
         # Look left
         or all((grid[row][col] > tree for tree in grid[row][col + 1:]))
+    )
+
+
+def get_best_scenic_score(trees_grid):
+    """Function to get the best tree's scenic score of the grid."""
+
+    best_scenic_score = 0
+
+    for row in range(1, len(trees_grid) - 1):
+        for col in range(1, len(trees_grid[row]) - 1):
+
+            current_tree_scenic_score = get_scenic_score(row, col, trees_grid)
+
+            if best_scenic_score < current_tree_scenic_score:
+                best_scenic_score = current_tree_scenic_score
+
+    return best_scenic_score
+
+
+def get_scenic_score(row, col, trees_grid):
+    """Function to get the given tree's scenic score on the given grid."""
+
+    # Look up
+    up_scenic_score = 0
+    for i in range(row - 1, -1, -1):
+        up_scenic_score += 1
+
+        if trees_grid[row][col] <= trees_grid[i][col]:
+            break
+
+    # Look down
+    down_scenic_score = 0
+    for i in range(row + 1, len(trees_grid)):
+        down_scenic_score += 1
+
+        if trees_grid[row][col] <= trees_grid[i][col]:
+            break
+
+    # Look left
+    left_scenic_score = 0
+    for i in range(col - 1, -1, -1):
+        left_scenic_score += 1
+
+        if trees_grid[row][col] <= trees_grid[row][i]:
+            break
+
+    # Look right
+    right_scenic_score = 0
+    for i in range(col + 1, len(trees_grid[row])):
+        right_scenic_score += 1
+
+        if trees_grid[row][col] <= trees_grid[row][i]:
+            break
+
+    return (
+        up_scenic_score
+        * down_scenic_score
+        * left_scenic_score
+        * right_scenic_score
     )
 
 
